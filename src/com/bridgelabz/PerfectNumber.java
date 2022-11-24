e7d6c4826f6d7a0b9cc5fe07db6bb2e92c368c4f
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-24 21:44:16 +0530

    Find the perfect number

---
+++ src/com/bridgelabz/PerfectNumber.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @GovardhanReddy
+ */
+public class PerfectNumber {
+    /**
+     * find the number is perfect number or not
+     * @param number
+     */
+    public static int perfectNumber(int number){
+        int sumOfFactorNumbers = 0;
+        for (int i = 1; i<=number/2;i++){
+            if (number%i==0){
+            sumOfFactorNumbers+=i;
+            }
+        }
+        System.out.println("sum of factors - "+sumOfFactorNumbers);
+        return sumOfFactorNumbers;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int number = input.nextInt();
+        int sumOfFactors =perfectNumber(number);
+        if (number==sumOfFactors){
+            System.out.println(number+" is perfect number");
+        }
+        else {
+            System.out.println(number+" is not perfect number");
+        }
+    }
+}
