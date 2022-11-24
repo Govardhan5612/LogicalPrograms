75e9b3da97318401d0baba0388a212cff0400863
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-24 21:09:53 +0530

    Fibonacci series

---
+++ src/com/bridgelabz/FibonacciSeries.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @GovardhanReddy
+ */
+
+public class FibonacciSeries {
+    /**
+     *compute the fibonacci series
+     */
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int number = input.nextInt();
+        int firstValue = 0;
+        int secondValue = 1;
+        int sumOfBeforeTwoValues;
+        System.out.print(firstValue+" "+secondValue+" ");
+        for (int i = 1;i<=number;i++){
+            sumOfBeforeTwoValues=firstValue+secondValue;
+            firstValue=secondValue;
+            secondValue=sumOfBeforeTwoValues;
+            System.out.print(sumOfBeforeTwoValues+" ");
+        }
+    }
+}
